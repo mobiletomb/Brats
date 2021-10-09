176033eec38846e1245a783a7a3c0bf41d854af3
qlc <1263591209@qq.com>
2021-10-09 10:24:08 +0800

    add infoNCE loss

--- metircs_losses.py
+++ metircs_losses.py
@@ -203,6 +203,56 @@ def jaccard_coef_metric_per_classes(probabilities: np.ndarray,
     return scores
 
 
-def InfoNCEloss():
-    loss = 0
-    return loss
+class NCECriterion(nn.Module):
+    def __init__(self, n_data):
+        super(NCECriterion, self).__init__()
+
+    def forward(self, x):
+        bsz = x.shape[0]
+        m = x.size(1) - 1
+        eps = 1e-5
+
+        # noise distribution
+        Pn = 1 / float(self.n_data)
+
+        # loss for positive pair
+        P_pos = x.select(1, 0)
+        log_D1 = torch.div(P_pos, P_pos.add(m * Pn + eps)).log_()
+
+        # loss for K negative pair
+        P_neg = x.narrow(1, 1, m)
+        log_D0 = torch.div(P_neg.clone().fill_(m * Pn), P_neg.add(m * Pn + eps)).log_()
+
+        loss = -(log_D1.sum(0) + log_D0.view(-1 , 1).sum(0)) / bsz
+
+        return loss
+
+
+class NCESoftmaxLoss(nn.Module):
+    "Softmax cross-entropy loss (a.k.a., info-NCE loss in CPC paper)"
+
+    def __init__(self):
+        super(NCESoftmaxLoss, self).__init__()
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        bsz = x.shape[0]
+        x = x.squeeze()
+        label = torch.zeros([bsz]).cuda().long()
+        loss = self.criterion(x, label)
+        return loss
+
+
+class NCEKLLoss(nn.Module):
+
+    def __init__(self):
+        super(NCEKLLoss, self).__init__()
+        self.criterion = nn.KLDivLoss()
+
+    def forward(self, x):
+        bsz = x.shape[0]
+        x = x.squeeze()
+        x = nn.functional.log_sofmax(x, dim=1)
+        label = torch.zeros([bsz]).cuda().long()
+        loss = self.criterion(x, label)
+        return loss
